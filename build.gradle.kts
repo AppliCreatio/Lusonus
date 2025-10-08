713ea713873678122e16749fe062ef2a0ca52d98
BrandonBoros <2377215@johnabbottcollege.net>
2025-10-08 17:30:00 -0400

    Added ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,34 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
+    // Gradle Android + Kotlin
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+
+    // Regular Kotlin JVM plugin (must include version!)
+    kotlin("jvm") version "1.8.10"
+
+    // Application plugin (for running Kotlin apps)
+    application
+
+    // Ktlint plugin
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
+}
+
+application {
+    mainClass.set("org.jlleitschuh.gradle.ktlint.sample.kotlin.MainKt")
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+    filter {
+        exclude("**/style-violations.kt")
+    }
 }
