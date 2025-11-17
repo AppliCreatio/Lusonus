a32b64e3ca4467301c89946edb68d7738d480ee2
AlexanderLystukha <alystukha@gmail.com>
2025-11-17 10:52:26 -0500

    Chamged application name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.emptyactivity"
+        applicationId = "com.example.lusonus"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
